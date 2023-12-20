e042b4e49b94124277ebb28cfd7c1d1f2eb3faa5
System Administrator <root@Juans-MacBook-Air.local>
2023-12-19 22:06:02 -0500

    hola

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Hall />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
       <Services/>
